16efe66a522f7958873a9b0a189299731b9cf323
JP Moregain <j.p.moregain15@gmail.com>
2023-08-21 10:07:45 -0400

    Add function for reading stats from Hero using bit manipulation

--- contracts/Hero.sol
+++ contracts/Hero.sol
@@ -6,11 +6,63 @@ contract Hero {
 
     mapping(address => uint[]) addressToHeroes;
 
+    function generateRandom() public view returns (uint) {
+        return uint(keccak256(abi.encodePacked(block.timestamp)));
+    }
+
     function getHeroes() public view returns (uint[] memory) {
         return addressToHeroes[msg.sender];
     }
 
-    function createHero(Class) public payable {
+    function getStrength(uint hero) public pure returns (uint) {
+        return (hero >> 2) & 0x1F;
+    }
+
+    function getHealth(uint hero) public pure returns (uint) {
+        return (hero >> 7) & 0x1F;
+    }
+
+    function getDex(uint hero) public pure returns (uint) {
+        return (hero >> 12) & 0x1F;
+    }
+
+    function getIntellect(uint hero) public pure returns (uint) {
+        return (hero >> 17) & 0x1F;
+    }
+
+    function getMagic(uint hero) public pure returns (uint) {
+        return (hero >> 22) & 0x1F;
+    }
+
+    function createHero(Class class) public payable {
         require(msg.value >= 0.05 ether, "Insufficient funds: Please send required payment");
+    
+        uint[] memory stats = new uint[](5);
+
+        stats[0] = 2;
+        stats[1] = 7;
+        stats[2] = 12;
+        stats[3] = 17;
+        stats[4] = 22;
+
+        uint len = 5;
+
+        uint hero = uint(class);
+
+        while (len > 0) {
+            uint position = generateRandom() % len;
+
+            // generate random value between 0 and 18
+            uint value = generateRandom() % (13 + len) + 1;
+
+            hero |= value << stats[position];
+
+            // decrement length to avoid choosing same stat again
+            len--;
+
+            stats[position] = stats[len];
+        }
+
+        addressToHeroes[msg.sender].push(hero);
     }
 }
